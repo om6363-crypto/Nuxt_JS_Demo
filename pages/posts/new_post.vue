e3b6f00a8e4321f40426ce9fda45fbd075a47bcf
Om Singh <om.singh@sportzinteractive.com>
2024-08-28 14:45:24 +0530

    Commit for new_commit

--- pages/posts/new_post.vue
+++ pages/posts/new_post.vue
@@ -1,5 +1,27 @@
 <template>
-    
+    <div>
+        <h1> Create a New Post</h1>
+        <form @onSubmit="submitForm">
+            <div>
+                <label for="id">ID</label>
+                <input type="number" id="id" v-model="form.id" @blur="validateId"/>
+                <span v-if="errors.id">{{ errors.id }}</span>
+            </div>
+            <div>
+                <label for="title">Title</label>
+                <input type="text" id="title" v-model="form.title" @blur="validateTitle"/>
+                <span v-if="errors.title">{{ errors.title }}</span>
+            </div>
+            <div>
+                <label for="body">Body</label>
+                <input type="text" id="body" v-model="form.body" @blur="validateBody"/>
+                <span v-if="errors.body">{{ errors.body }}</span>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+
+    <p v-if="successMessage">{{ successMessage }}</p>
+</div>
 </template>
 
 <script>
@@ -9,7 +31,7 @@ export default {
             form: {
                 title: '',
                 body: '',
-                userId: null
+                id: null
             },
             errors: {},
             successMessage: ''
@@ -36,7 +58,7 @@ export default {
             if(!this.form.id){
                 this.errors.id = 'Id is required'
             }
-            else if(isNAN(this.form.id)){
+            else if(isNaN(this.form.id)){
                 this.errors.id = 'User ID must be a number is required'
             }
             else{
@@ -44,6 +66,7 @@ export default {
             }
         }
     },
+
     async submitForm(){
         this.validateId(),
         this.validateBody(),
@@ -51,10 +74,19 @@ export default {
         if(Object.keys(this.errors).length === 0){
             try{
                 const response = await this.$axios.$post('https://jsonplaceholder.typicode.com/posts', this.form)
-                console.log(response)
+                if(response.status === 201){
+                    successMessage="Post created successfully"
+                    console.log(response.data);
+                    console.log("Form Data----", JSON.stringify(this.form))
+                    this.form={
+                        title:'',
+                        body:'',
+                        id: null
+                    }
+                }
             }
             catch(error){
-                
+                console.log("Error Submitting Form", error.message)
             }
         }
     }
